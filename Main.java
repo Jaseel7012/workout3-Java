71540321ba41cc9f6f6baa4d70a096b4d7d363c4
jaseel <jaseeljctcs@gmail.com>
2022-09-23 00:49:43 +0530

    deleted

--- Main.java
+++ Main.java
@@ -1,5 +1,31 @@
 public class Main {
+    int f=9;
+    static void eat(){
+        System.out.println("PROCESSING");
+
+
+
+    }
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        String[] frts={"papaya","banana","apple"};
+        for (String i:frts){
+            System.out.println(i);
+        }
+        for(int i=1;i<13;i++){
+            if (i==6){
+                continue;
+            }
+            System.out.println(i);
+
+
+
+        }
+        Main myobj=new Main();
+
+        System.out.println(myobj.f);
+        myobj.eat();
+
+
     }
 }
